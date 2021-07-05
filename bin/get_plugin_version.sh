5790a8d63e0948e7857117b87124b9738165c00c
Thomas Sjolshagen <thomas@eighty20results.com>
2021-07-05 08:13:48 +0200

    BUG FIX: Wrong path for source file

--- bin/get_plugin_version.sh
+++ bin/get_plugin_version.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 plugin_shortname="${1}"
-grep -E "^Version:" "../class-${plugin_shortname}.php" | \
+grep -E "^Version:" "class-${plugin_shortname}.php" | \
 	sed 's/[[:alpha:]|(|[:space:]|\:]//g' | \
 	awk -F- '{printf "%s", $1}'
