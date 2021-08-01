13caaa5d2d523db43a616456126a01bd8f0987f9
Thomas Sjolshagen <thomas@eighty20results.com>
2021-08-01 10:28:54 +0200

    BUG FIX: Renamed Exception from ConfigFileNotFound() to ConfigDataNotFound()

--- src/licensing/exceptions/class-configdatanotfound.php
+++ src/licensing/exceptions/class-configdatanotfound.php
@@ -24,7 +24,7 @@ namespace E20R\Utilities\Licensing\Exceptions;
 use Exception;
 use Throwable;
 
-class ConfigFileNotFound extends Exception {
+class ConfigDataNotFound extends Exception {
 	public function __construct( string $message = '', int $code = 0, ?Throwable $previous = null ) {
 		parent::__construct( $message, $code, $previous );
 	}
