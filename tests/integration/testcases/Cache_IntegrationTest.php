13579cc144e0bce19d1b6e5b298f668fb5e56051
Thomas Sjolshagen <thomas@eighty20results.com>
2022-01-21 07:58:51 +0100

    BUG FIX: Handle different versions of the (mostly) happy path testing for the Cache::set()/Cache::get() methods

--- tests/integration/testcases/Cache_IntegrationTest.php
+++ tests/integration/testcases/Cache_IntegrationTest.php
@@ -33,16 +33,37 @@ use E20R\Utilities\Utilities;
 class Cache_IntegrationTest extends WPTestCase {
 
 	/**
+	 * Instance of the Utilities class
+	 *
+	 * @var null|Utilities $utils
+	 */
+	private $utils = null;
+
+	/**
 	 * Initial setup for the test cases
 	 */
-	public function setUp(): void {
+	public function setUp() : void {
 		parent::setUp();
+
 		$message     = new Message();
 		$this->utils = new Utilities( $message );
+
+		// Force us to use the DB for testing purposes
+		wp_using_ext_object_cache( false );
 	}
 
 	/**
-	 * Fixture for the it_should_get_set() test
+	 * The tear-down function for the Codeception Integration tests
+	 *
+	 * @return void
+	 */
+	public function tearDown(): void {
+		$this->utils = null;
+		parent::tearDown();
+	}
+
+	/**
+	 * Fixture for the it_should_add_single_cached_object_to_options_table_in_db() test
 	 *
 	 * @return array[]
 	 */
@@ -56,7 +77,7 @@ class Cache_IntegrationTest extends WPTestCase {
 			array( 1, 'e20r_tst6', 10, true, 1 ),
 			array( 10, 'e20r_tst7', null, true, 1 ),
 			array( md5( 'something' ), 'e20r_tst8', 'any_value', true, 1 ),
-			array( md5( 'something', true ), 'e20r_tst9', 'any_other_value', true, 1 ),
+			array( md5( 'something', true ), 'e20r_tst9', 'any_other_value', false, 0 ),
 		);
 	}
 
@@ -79,14 +100,57 @@ class Cache_IntegrationTest extends WPTestCase {
 		$cache  = new Cache( $key_name, $group_name );
 		$result = false;
 		try {
-			$result = $cache->set_data( $key_name, $to_value, 10, $group_name );
+			$result = $cache->set_data( $key_name, $to_value, 3600, $group_name );
 		} catch ( BadOperation $e ) {
-			self::assertFalse( true, 'Error: Unexpected BadOperation() exception caught in this test!' );
+			self::assertFalse( true, 'Error: ' . $e->getMessage() );
 		}
 		$real_count = $this->get_cache_count_from_db( $key_name, $group_name );
 
-		self::assertSame( $count, $real_count );
-		self::assertSame( $expected, $result );
+		self::assertSame( $count, $real_count, "Error: The count for the key {$key_name} and group {$group_name} cache is {$real_count} vs expected {$count}" );
+		self::assertSame( $expected, $result, "Error: The result from Cache::set_data( {$key_name}, 'any_value', 3600, $group_name) is: {$real_count} vs expected: {$count}" );
+	}
+
+	/**
+	 * Fixture for the it_should_fail_to_add_to_options_table_in_db() test
+	 *
+	 * @return array[]
+	 */
+	public function fixture_individual_key_group_failures() {
+		return array(
+			// FIXME: Test that the expected exceptions are triggered!
+			array( md5( 'something', true ), 'e20r_tst1', 'any_other_value', false, 0 ),
+		);
+	}
+
+	/**
+	 * Test whether setting the cache object property is happening properly when the class is instantiated
+	 *
+	 * @param string $key_name   The name of the object property to set
+	 * @param string $group_name The cache group name to use
+	 * @param string $to_value   The value to set the cache to
+	 * @param mixed  $expected   The resulting value from the set() operation
+	 * @param int    $count      The resulting count of _transient_* records in the $wpdb->prefix_options table
+	 *
+	 * @dataProvider fixture_individual_key_group_failures
+	 *
+	 * @return void
+	 * @test
+	 *
+	 * FIXME: Test that the expected exceptions are triggered!
+	 */
+	public function it_should_fail_to_add_to_options_table_in_db( $key_name, $group_name, $to_value, $expected, $count ) {
+
+		$cache  = new Cache( $key_name, $group_name );
+		$result = false;
+		try {
+			$result = $cache->set_data( $key_name, $to_value, 3600, $group_name );
+		} catch ( BadOperation $e ) {
+			self::assertFalse( true, 'Error: ' . $e->getMessage() );
+		}
+		$real_count = $this->get_cache_count_from_db( $key_name, $group_name );
+
+		self::assertSame( $count, $real_count, "Error: The count for the key {$key_name} and group {$group_name} cache is {$real_count} vs expected {$count}" );
+		self::assertSame( $expected, $result, "Error: The result from Cache::set_data( {$key_name}, 'any_value', 3600, $group_name) is: {$real_count} vs expected: {$count}" );
 	}
 
 	/**
@@ -99,11 +163,141 @@ class Cache_IntegrationTest extends WPTestCase {
 	 */
 	private function get_cache_count_from_db( $key, $group ) {
 		global $wpdb;
-		return (int) $wpdb->get_results( // phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
+		$transient_label = "_transient_{$group}_{$key}" . '%';
+		// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
+		return (int) $wpdb->get_var(
 			$wpdb->prepare(
-				"SELECT COUNT(option_id) FROM {$wpdb->options} WHERE option_name LIKE %s",
-				"_transient_{$group}_{$key}" . '%'
+				"SELECT COUNT(option_id) AS found_entries FROM {$wpdb->options} WHERE option_name LIKE %s",
+				$transient_label
 			)
 		);
 	}
+
+	/**
+	 * Fixture for the it_should_add_multiple_cache_objects_to_cache_group() test
+	 *
+	 * @return array[]
+	 */
+	public function fixture_multiple_keys_per_group() {
+		$fixture = array();
+
+		foreach ( range( 0, 5 ) as $group_no ) {
+			$fixture[ $group_no ] = array();
+			foreach ( range( 1, 10 ) as $counter ) {
+				$fixture[ $group_no ][] = array( "key_{$counter}", true, $counter );
+			}
+		}
+		return array( $fixture );
+	}
+
+	/**
+	 * Test multiple cache set() operations for distinct keys but the same group name.
+	 *
+	 * @param array[] $test_group The group of cache key/value pairs and results to test/test for
+	 *
+	 * @return void
+	 *
+	 * @dataProvider fixture_multiple_keys_per_group
+	 * @test
+	 */
+	public function it_should_add_multiple_cache_objects_to_single_cache_group( $test_group ) {
+		$group_name = 'moicg';
+
+		/**
+		 * Iterates through the array of test variables used for multiple cache keys
+		 * and a single group (default or otherwise)
+		 *
+		 * @param string $key Is the Cache Key to use
+		 * @param bool   $expected Is the expected return value from the Cache::set_data() or Cache::set() method
+		 * @param int    $count    Is the expected number of entries in the cache
+		 */
+		foreach ( $test_group as $group => $item ) {
+			$key      = $item[0];
+			$expected = $item[1];
+			$count    = $item[2];
+			$cache    = new Cache( $key, $group_name );
+
+			$result = false;
+			try {
+				$result = $cache->set_data( $key, 'any_value', 3600, $group_name );
+			} catch ( BadOperation $e ) {
+				self::assertFalse( true, 'Error: ' . $e->getMessage() );
+			}
+			$real_count = $this->get_group_count_from_db( $group_name );
+
+			self::assertSame( $count, $real_count, "Error: The count for the key {$key} and group {$group_name} cache is {$real_count} vs expected {$count}" );
+			self::assertSame( $expected, $result, "Error: The result from Cache::set_data( {$key}, 'any_value', 3600, $group_name) is: {$real_count} vs expected: {$count}" );
+		}
+	}
+
+	/**
+	 * Test multiple cache set() operations for distinct keys but the same group name.
+	 *
+	 * @param array[] $test_group The name of the object property to set
+	 *
+	 * @return void
+	 *
+	 * @dataProvider fixture_multiple_keys_per_group
+	 * @test
+	 */
+	public function it_should_add_multiple_cache_objects_to_default_cache_group( $test_group ) {
+
+		/**
+		 * Iterates through the array of test variables used for multiple cache keys
+		 * and a single group (default or otherwise)
+		 *
+		 * @param string $key Is the Cache Key to use
+		 * @param bool   $expected Is the expected return value from the Cache::set_data() or Cache::set() method
+		 * @param int    $count    Is the expected number of entries in the cache
+		 */
+		foreach ( $test_group as $group => $item ) {
+			$key        = $item[0];
+			$expected   = $item[1];
+			$count      = $item[2];
+			$cache      = new Cache( $key );
+			$result     = false;
+			$group_name = '';
+
+			try {
+				$result = $cache->set_data( $key, 'any_value' );
+			} catch ( BadOperation $e ) {
+				self::assertFalse( true, 'Error: ' . $e->getMessage() );
+			}
+
+			// Fetch the default cache group name from the Cache() class
+			try {
+				$group_name = $cache->property_get( 'group' );
+			} catch ( BadOperation $e ) {
+				self::assertFalse( true, 'Error: ' . $e->getMessage() );
+			}
+
+			self::assertSame( 'e20r_group', $group_name, "Error: Expected a group name of 'e20r_group' but got {$group_name} instead!" );
+			$real_count = $this->get_group_count_from_db( $group_name );
+
+			self::assertSame( $count, $real_count, "Error: The count for the key {$key} and group {$group_name} cache is {$real_count} vs expected {$count}" );
+			self::assertSame( $expected, $result, "Error: The result from Cache::set_data( {$key}, 'any_value', 3600, $group_name) is: {$real_count} vs expected: {$count}" );
+
+		}
+	}
+
+	/**
+	 * Private function to fetch data from wp_options when testing Cache() class
+	 *
+	 * @param string|null $group The cache group name to use
+	 *
+	 * @return int
+	 */
+	private function get_group_count_from_db( $group ) {
+		global $wpdb;
+		$transient_label = "_transient_{$group}_" . '%';
+
+		// phpcs:ignore WordPress.DB.DirectDatabaseQuery.DirectQuery, WordPress.DB.DirectDatabaseQuery.NoCaching
+		return (int) $wpdb->get_var(
+			$wpdb->prepare(
+				"SELECT count(option_id) FROM {$wpdb->options} WHERE option_name LIKE %s",
+				$transient_label
+			)
+		);
+	}
+
 }
